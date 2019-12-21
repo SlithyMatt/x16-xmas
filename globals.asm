54f7f67603c9e5ce09b50cad816246269a51bd52
SlithyMatt <slithymatt@hotmail.com>
2019-12-21 02:19:18 -0500

    Refactor for r35

--- globals.asm
+++ globals.asm
@@ -11,7 +11,7 @@ VRAM_TEXT            = $00000
 VRAM_BITMAP          = $04000 ; 8bpp 320x240
 VRAM_CHARACTER_SET   = $1F800
 
-MUSIC_BANK     = 0
+MUSIC_BANK     = 41
 BM1_BANK       = 1
 BM2_BANK       = 11
 BM3_BANK       = 21
